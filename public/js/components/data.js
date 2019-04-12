8369c7b056125b7acdf3e1e558041762121be8c7
iplayguitar81 <iplayguitar81@yahoo.com>
2019-04-11 22:23:13 -0700

    committing API routes

---
+++ public/js/components/data.js
@@ -0,0 +1,15 @@
+export default {
+    data() {
+        return {
+            articles: [],
+            article: {
+                id: '',
+                title: '',
+                body: ''
+
+            },
+
+        }
+    }
+
+}
