c8ed85e1b87c50ba17f09ac90afb78b021c4fbcf
iplayguitar81 <iplayguitar81@yahoo.com>
2019-04-12 00:04:33 -0700

    committing API routes

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -33,7 +33,7 @@
         </articles>
 
 
-        <div class="card card-body" v-for="article in articles" v-bind:key="article.id"><h3>@{{article.title}}</h3><p>@{{ article.body }}</p></div>
+        <div class="card card-body mb-2" v-for="article in articles" v-bind:key="article.id"><h3>@{{article.title}}</h3><p>@{{ article.body }}</p></div>
 
     </div>
 
