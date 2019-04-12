54e587a1f480b7b8e2deede49fcdd020ee214a32
iplayguitar81 <iplayguitar81@yahoo.com>
2019-04-12 00:13:46 -0700

    committing API routes

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -30,6 +30,9 @@
 
         <articles></articles>
 
+
+        {{--<div class="card card-body mb-2" v-for="article in articles" v-bind:key="article.id"><h3>@{{article.title}}</h3><p>@{{ article.body }}</p></div>--}}
+
     </div>
 
 </div>
@@ -48,7 +51,31 @@
 <script>
 
     const app = new Vue({
-        el: '#app'
+        el: '#app',
+
+//        data: {
+//                articles: [],
+//                article: {
+//                    id: '',
+//                    title: '',
+//                    body: ''
+//        },
+//            article_id: '',
+//            pagination: {},
+//            edit: false
+//
+//        },
+//        created: function(){ this.fetchArticles();},
+//        methods: {
+//           fetchArticles(){
+//               fetch('api/articles')
+//                   .then(res => res.json())
+//               .then(res => {
+//                 this.articles=res.data;
+//                   //console.log(res.data);
+//               })
+//            }
+//        },
     });
 
 
