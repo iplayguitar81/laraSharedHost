36e0239fa99bf0ff3d463dc19350b6145efee615
iplayguitar81 <iplayguitar81@yahoo.com>
2019-04-11 23:55:48 -0700

    committing API routes

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -78,19 +78,10 @@
                })
             }
         },
-        {{--template: '<div class="card card-body" v-for="article in articles" v-bind:key="article.id"><h3>{{article.title}}</h3></div>'--}}
     });
 
 
-//    Vue.component("articles", {
-//
-//            template: "<div> <h2>Articles</h2> <div class='card card-body' v-for='article in articles' v-bind:key='article.id'><h3>@{{article.title}}</h3></div></div>",
-//
-//
-//        }
-//
-//
-//    );
+
 
 
 </script>
