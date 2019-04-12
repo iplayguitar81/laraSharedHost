7e6f9d9de43ceab154c8b5c49e702eb417bd5f2c
iplayguitar81 <iplayguitar81@yahoo.com>
2019-04-12 00:37:42 -0700

    committing API routes

--- public/js/components/Articles.vue.js
+++ public/js/components/Articles.vue.js
@@ -1,30 +1,7 @@
 Vue.component("articles", {
-        // props: ['articles'],
-        template: '<div> <h2>Articles</h2> <div class="card card-body" v-for="article in fetchArticles" v-bind:key="article.id"><h3>@{{article.title}}</h3></div></div>',
 
-    props: {
-        articles: [],
-        article: {
-            id: '',
-            title: '',
-            body: ''
-        },
-        article_id: '',
-        pagination: {},
-        edit: false
+        template: '<div> <h2>Articles</h2> <div class="card card-body" v-for="article in articles" v-bind:key="article.id"><h3>@{{article.title}}</h3></div></div>',
 
-    },
-    // created: function(){ this.fetchArticles();},
-    computed: {
-        fetchArticles(){
-            fetch('api/articles')
-                .then(res => res.json())
-        .then(res => {
-                this.articles=res.data;
-            //console.log(res.data);
-        })
-        }
-    },
 
     }
 
