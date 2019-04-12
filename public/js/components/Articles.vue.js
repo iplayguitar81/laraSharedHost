51c3eca6ef90b96f97fe16fe0de7fb73b2dcef67
iplayguitar81 <iplayguitar81@yahoo.com>
2019-04-11 23:04:57 -0700

    committing API routes

--- public/js/components/Articles.vue.js
+++ public/js/components/Articles.vue.js
@@ -2,7 +2,7 @@
 
 Vue.component("articles", {
 
-    template: '<div> <h2>Articles</h2><div class="card card-body" v-for="article in articles" v-bind:key="article.id"><h3>{{article.title}}</h3></div></div>'
+    template: '<div> <h2>Articles</h2></div>'
 
     }
 
