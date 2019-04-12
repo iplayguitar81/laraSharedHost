ed4d91f74b0515015df874f4ac3d64bfe9182e2a
iplayguitar81 <iplayguitar81@yahoo.com>
2019-04-11 20:46:58 -0700

    committing API routes

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,1 +1,26 @@
-test this out...
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>LaraVue</title>
+
+    <!-- Fonts -->
+
+
+    <!-- Styles -->
+
+
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js"></script>
+</head>
+<body>
+
+<div id="app">
+
+
+</div>
+
+
+</body>
+</html>
