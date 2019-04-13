2a6c62895e25c4ac38b0464f8280246b1dad9558
iplayguitar81 <iplayguitar81@yahoo.com>
2019-04-12 20:57:57 -0700

    committing API routes

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -36,6 +36,8 @@
             <ul class="pagination">
                 <li v-bind:class="[{disabled: !pagination.prev_page_url}]" class="page-item"><a class="page-link" href="#" @click="fetchArticles(pagination.prev_page_url)">Previous</a></li>
 
+                <li class="page-item disabled"><a class="page-link text-dark" href="#">Page @{{pagination.current_page}} of @{{pagination.last_page }}</a></li>
+
                 <li v-bind:class="[{disabled: !pagination.next_page_url}]" class="page-item"><a class="page-link" href="#" @click="fetchArticles(pagination.next_page_url)">Next</a></li>
             </ul>
         </nav>
