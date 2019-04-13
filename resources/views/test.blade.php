f142bf91d91a15028b66fad8f99d96bacba56de8
iplayguitar81 <iplayguitar81@yahoo.com>
2019-04-12 20:43:55 -0700

    committing API routes

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -34,10 +34,8 @@
 
         <nav aria-label="Page navigation example">
             <ul class="pagination">
-                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li v-bind:class="[{disabled: !pagination.prev_page_url}]" class="page-item"><a class="page-link" href="#">Previous</a></li>
+
                 <li class="page-item"><a class="page-link" href="#">Next</a></li>
             </ul>
         </nav>
