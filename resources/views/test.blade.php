d32788b6a5f437f8c4bbc1dd599dd8fda1121be8
iplayguitar81 <iplayguitar81@yahoo.com>
2019-04-12 21:43:42 -0700

    committing API routes

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -32,7 +32,7 @@
 
         </articles>
 
-        <form class="mb-3">
+        <form @submit.prevent="addArticle()" class="mb-3">
             <div class="form-group">
                 <input type="text" class="form-control" placeholder="Title" v-model="article.title">
             </div>
