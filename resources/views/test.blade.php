15bd2aac5ee7c597f70cac9699015ea85ed3720a
iplayguitar81 <iplayguitar81@yahoo.com>
2019-04-12 20:39:43 -0700

    committing API routes

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -32,6 +32,15 @@
 
         </articles>
 
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+            </ul>
+        </nav>
 
         <div class="card card-body mb-2" v-for="article in articles" v-bind:key="article.id"><h3>@{{article.title}}</h3><p>@{{ article.body }}</p></div>
 
