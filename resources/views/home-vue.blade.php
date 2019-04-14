1f2661e91f90d4ba50098383f75be3a264fa2625
iplayguitar81 <iplayguitar81@yahoo.com>
2019-04-14 00:22:07 -0700

    committing API routes

--- resources/views/home-vue.blade.php
+++ resources/views/home-vue.blade.php
@@ -44,15 +44,15 @@
             {{--<button type="submit" class="btn btn-md btn-success">Save</button>--}}
         {{--</form>--}}
 
-        <nav aria-label="Page navigation example">
-            <ul class="pagination">
-                <li v-bind:class="[{disabled: !pagination.prev_page_url}]" class="page-item"><a class="page-link" href="#" @click="fetchArticles(pagination.prev_page_url)">Previous</a></li>
+        {{--<nav aria-label="Page navigation example">--}}
+            {{--<ul class="pagination">--}}
+                {{--<li v-bind:class="[{disabled: !pagination.prev_page_url}]" class="page-item"><a class="page-link" href="#" @click="fetchArticles(pagination.prev_page_url)">Previous</a></li>--}}
 
-                <li class="page-item disabled"><a class="page-link text-dark" href="#">Page @{{pagination.current_page}} of @{{pagination.last_page }}</a></li>
+                {{--<li class="page-item disabled"><a class="page-link text-dark" href="#">Page @{{pagination.current_page}} of @{{pagination.last_page }}</a></li>--}}
 
-                <li v-bind:class="[{disabled: !pagination.next_page_url}]" class="page-item"><a class="page-link" href="#" @click="fetchArticles(pagination.next_page_url)">Next</a></li>
-            </ul>
-        </nav>
+                {{--<li v-bind:class="[{disabled: !pagination.next_page_url}]" class="page-item"><a class="page-link" href="#" @click="fetchArticles(pagination.next_page_url)">Next</a></li>--}}
+            {{--</ul>--}}
+        {{--</nav>--}}
 
         <div class="card card-body mb-2" v-for="article in articles" v-bind:key="article.id">
             <h3>@{{article.title}}</h3>
