bb7e7e04ff03dee92537c5354c122475d72e7f2f
iplayguitar81 <iplayguitar81@yahoo.com>
2019-04-13 23:20:57 -0700

    committing API routes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/spa-crud';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/spa-crud';
 
     /**
      * Create a new controller instance.
